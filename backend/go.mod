9ff1cc8ae6f935984e4e23348c5efc78304eed67
Rishikesh Suvarna <rishikeshsuvarna@gmail.com>
2024-10-06 16:36:38 +0100

    - feat: Added types and interfaces for Threads, Posts and Comments

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,7 @@
 module github.com/rishikesh-suvarna/go-reddit
 
 go 1.22.5
+
+require github.com/pborman/uuid v1.2.1
+
+require github.com/google/uuid v1.6.0 // indirect
---
+++ backend/types.go
@@ -0,0 +1,48 @@
+package main
+
+import "github.com/pborman/uuid"
+
+type Thread struct {
+	ID          uuid.UUID `db:"id"`
+	Title       string    `db:"title"`
+	Description string    `db:"description"`
+}
+
+type Post struct {
+	ID       uuid.UUID `db:"id"`
+	ThreadID uuid.UUID `db:"thread_id"`
+	Title    string    `db:"title"`
+	Content  string    `db:"content"`
+	Votes    int       `db:"votes"`
+}
+
+type Comment struct {
+	ID      uuid.UUID `db:"id"`
+	PostID  uuid.UUID `db:"post_id"`
+	Content string    `db:"content"`
+	Votes   int       `db:"votes"`
+}
+
+type ThreadStore interface {
+	Thread(id uuid.UUID) (Thread, error)
+	Threads() ([]Thread, error)
+	CreateThread(thread *Thread) error
+	UpdateThread(thread *Thread) error
+	DeleteThread(id uuid.UUID) error
+}
+
+type PostStore interface {
+	Post(id uuid.UUID) (Post, error)
+	Posts() ([]Post, error)
+	CreatePost(post *Post) error
+	UpdatePost(post *Post) error
+	DeletePost(id uuid.UUID) error
+}
+
+type CommentStore interface {
+	Comment(id uuid.UUID) (Comment, error)
+	Comments() ([]Comment, error)
+	CreateComment(comment *Comment) error
+	UpdateComment(comment *Comment) error
+	DeleteComment(id uuid.UUID) error
+}
